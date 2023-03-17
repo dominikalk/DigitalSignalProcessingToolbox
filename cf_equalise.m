261370daba49a896cb1b7a8b9280ae30dddd4926
Dominik Alkhovik <dominik.alk@hotmail.com>
2023-03-17 22:49:33 +0000

    add multiple band equaliser

--- cf_equalise.m
+++ cf_equalise.m
@@ -1,3 +1,29 @@
 function ss = cf_equalise(s, b)
-    ss = s;
+
+    % add check to make sure b is correct length
+
+    bf = [16, 31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
+
+    y = s.y;
+    Fs = s.Fs;
+    Q = 3;
+
+    % Apply Base_Shelf filter to lowest band
+    % Shelving filter from https://uk.mathworks.com/matlabcentral/fileexchange/16568-bass-treble-shelving-filter
+    [c, a] = shelving(b(1), bf(1), Fs, Q, 'Base_Shelf');
+    y = filter(c, a, y);
+
+    % Apply peaking filter for middle bands
+    % Peaking filter from https://uk.mathworks.com/matlabcentral/fileexchange/16567-peaking-notch-iir-filter
+    for i = 2:10
+        [c, a] = peaking(b(i), bf(i), Q, Fs);
+        y = filter(c, a, y);
+    end
+
+    % Apply Base_Treble filter to lowest band
+    % Shelving filter from https://uk.mathworks.com/matlabcentral/fileexchange/16568-bass-treble-shelving-filter
+    [c, a] = shelving(b(11), bf(11), Fs, Q, 'Treble_Shelf');
+    y = filter(c, a, y);
+
+    ss = struct("y", y, "Fs", Fs);
 end
--- cf_ext_a.m
+++ cf_ext_a.m
@@ -1,3 +1,59 @@
-function cf_ext_a()
+function ss = cf_ext_a(s)
+    Fs = s.Fs;
+    x = s.y;
+    
+    % EFFECT COEFFICIENTS
+    % Damping factor: the lower the smaller the pass band
+    damp = 0.05;
+    % Min and max centre cutoff frequency of variable bandpass filter
+    minf=300;
+    maxf=3000;
+    % wah frequency, how many Hz per secondare cycled through
+    Fw = 1;
+    
+    % Create triangle wave
+    Centre_freq = (maxf + minf)/2;
+    offset = maxf - Centre_freq;
+    Fc = Centre_freq + offset*sin(2*pi*(1:length(x))*Fw/Fs);
+    
+    figure();
+    plot(Fc);
+    title('Modulating Frequency Plot');
 
+    % Difference equation coefficients
+    F1 = 2*sin((pi*Fc(1))/Fs);  % must be recalculated each time Fc changes
+    Q1 = 2*damp;                % this dictates size of the pass bands
+    
+    yh = zeros(size(x));        % create empty out vectors
+    yb = zeros(size(x));
+    yl = zeros(size(x));
+    
+    % First sample, to avoid referencing of negative signals
+    yh(1) = x(1);
+    yb(1) = F1*yh(1);
+    yl(1) = F1*yb(1);
+    
+    % Apply difference equation to the sample
+    for n=2:length(x)
+      yh(n) = x(n) - yl(n-1) - Q1*yb(n-1);
+      yb(n) = F1*yh(n) + yb(n-1);
+      yl(n) = F1*yb(n) + yl(n-1);
+    
+      F1 = 2*sin((pi*Fc(n))/Fs);
+    end
+    
+    % Normalise
+    maxyb = max(abs(yb));
+    yb = yb/maxyb;
+    
+    % Plot
+    figure()
+    hold on
+    plot(x,'r');
+    plot(yb,'g');
+    title('Wah-wah and original Signal (Blue - Wah-Wah, Red - original)');
+    
+    sound(yb, Fs);
+
+    ss = struct("y", yb, "Fs", Fs);
 end
